733d38afe51b2d49eed34b4caf1cc9d848a3c5f0
Angel Marcos <angelmarcos@latamready.com>
2022-10-14 17:16:29 -0500

    DAL - creacion string conexion

---
+++ DAL/DbAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DbAccess
+    {
+        SqlConnection conexion = new SqlConnection("data source=.;database=senaticuros;user id=sa;password=123456;integrated security=false");
+
+        public SqlConnection getConexion()
+        {
+            return conexion;
+        }
+
+        public void abrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
