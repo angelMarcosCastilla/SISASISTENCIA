6f93db347f88471f1036db994dd84aad97988d70
angelMarcosCastilla <ajmarcosc15@gmail.com>
2022-10-19 21:47:21 -0500

    DESIGNER - inicio de la estructura de dashboard

--- DESIGNER/Dashboard.Designer.cs
+++ DESIGNER/Dashboard.Designer.cs
@@ -28,35 +28,109 @@
       /// </summary>
       private void InitializeComponent()
       {
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelForm = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Rockwell", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(276, 119);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(210, 42);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Bienvenido";
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 554);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 122);
+            this.panel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(200, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(867, 57);
+            this.panel3.TabIndex = 5;
+            // 
+            // panelForm
+            // 
+            this.panelForm.BackColor = System.Drawing.SystemColors.Info;
+            this.panelForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelForm.Location = new System.Drawing.Point(200, 57);
+            this.panelForm.Name = "panelForm";
+            this.panelForm.Size = new System.Drawing.Size(867, 497);
+            this.panelForm.TabIndex = 6;
             // 
             // Dashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.panelForm);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Dashboard";
             this.Text = "Dashboard";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Dashboard_FormClosing);
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
       }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelForm;
     }
 }
--- DESIGNER/Dashboard.cs
+++ DESIGNER/Dashboard.cs
@@ -12,6 +12,7 @@ namespace DESIGNER
 {
    public partial class Dashboard : Form
    {
+        private Form formularioActual;
       public Dashboard()
       {
          InitializeComponent();
@@ -20,6 +21,28 @@ namespace DESIGNER
         private void Dashboard_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void OpenChildForm(Form childForm)
+        {
+            if (formularioActual != null)
+            {
+                formularioActual.Close();
+            }
+            formularioActual = childForm;
+            formularioActual.TopLevel = false;
+            formularioActual.FormBorderStyle = FormBorderStyle.None;
+            formularioActual.Dock = DockStyle.Fill;
+            panelForm.Controls.Add(childForm);
+            panelForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+
         }
     }
 }
